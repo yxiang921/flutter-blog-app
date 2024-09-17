bb1ef25454184bc43bafb7b037c8c54808798787
yxiang921 <xiangf35@gmail.com>
2024-09-17 16:05:13 +0800

    Update: Add some filed

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -8,11 +10,56 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  void _submit() {}
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Sign Up Page'),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('Sign Up',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                icon: Icon(Icons.email),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+                icon: Icon(Icons.lock),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+          ),
+          
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Confirm Password',
+                icon: Icon(Icons.lock),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+          ),
+          
+          SizedBox(height: 20),
+          
+        ]),
       ),
     );
   }
